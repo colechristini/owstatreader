63dd50a06f8d921d17935f08f584ec238449dac0
Cole Christini <cole.christini@gmail.com>
2020-09-11 09:50:05 -0400

    wrote server+frame extraction

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const https = require('https');
+const bodyParser = require('body-parser');
+const statreader = require('./statread.js');
+const port = process.env.PORT || 8080; //default to port 8080, but uses https, doesn't work on 443let drivers = [];
+const fields = ['file', 'timestamps'];
+const opts = { fields };
+
+(async function () {
+    https.createServer({ //Initialize server
+        key: fs.readFileSync('ssh/key.pem'),
+        cert: fs.readFileSync('ssh/cert.pem'),
+        passphrase: 'perow59~'
+    }, app)
+        .listen(port);//load certificate and create secure server
+    app.use(bodyParser.json());//setup parser
+    console.log("Server up and running at port: " + port);
+})();
+// POST method route
+app.post('/uploadVideo', function (req, res) { //add new driver
+    let result = statreader.extractKeyframes(req.query.file, req.query.timestamps);
+    res.send(result);
+});
